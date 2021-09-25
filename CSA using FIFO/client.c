ff3ebe4821c54460336a7f795212502ba85536a1
Hollbrok <talashkevich.da@phystech.edu>
2021-09-26 02:03:54 +0300

    Need to fix some bugs with send/receive data

--- CSA using FIFO/client.c
+++ CSA using FIFO/client.c
@@ -10,6 +10,9 @@
 #define CLIENT_FIFO_NAME_LEN (sizeof(CLIENT_FIFO_TEMPLATE) + 20)    /* Space required for client FIFO pathname
                                                                     (+20 as a generous allowance for the PID) */
 
+static char clientFifo[CLIENT_FIFO_NAME_LEN];
+
+
 struct request      /* Request (client --> server) */       
 {                 
     pid_t pid;      /* PID of client */
@@ -26,6 +29,15 @@ struct response     /* Response (server --> client) */
 
 
 
+/// ALL INFO AT THE TOP CAN BE moved to a separate file (CSA_info) 
+
+static void /* Invoked on exit to delete client FIFO */
+removeFifo(void)
+{
+    unlink(clientFifo);
+}
+
+
 //////////////////////
 
 enum ERRORS_HANDLER
@@ -33,18 +45,17 @@ enum ERRORS_HANDLER
     NOT_ENOUGH_ARGUMENTS = -5,
     TO_MUCH_ARGUMENTS       ,
     ZEROSTRING_ARGV         ,
-    MKFIFO_NO_EEXIT         ,
+    MKFIFO_NO_EEXIT            ,
     OPEN_FIX_FD             ,
     SIGPIPE_IGN_ERROR       ,
     WRITE_TO_CLIENT         ,
-    ERROR_OPEN_TO_READ_CLIENT,
 };
 
 long getNumber(const char *numString);
 
 int main(int argc, const char *argv[])
 {
-    /*
+
     if (argc < 2)
     {
         fprintf(stderr, "Program needs 2 arguments\n");
@@ -62,101 +73,76 @@ int main(int argc, const char *argv[])
         fprintf(stderr, "zero string argv2\n");
         exit(ZEROSTRING_ARGV);
     }
-    */
 
 /// 
 
     int serverFd = -1;      // read client request
     int clientFd = -1;      // read server response
 
-    int fixFd  = -1;        // this is FD to fix problem with FIFO: it needs to open FIFO write end to not meet EOF.
-
     struct request req;     // request to server from client
     struct response resp;   // answer to client from server
 
-    char clientFifo[CLIENT_FIFO_NAME_LEN];
+///
 
-//
+    /* Create our FIFO (before sending request, to avoid a race) */
 
-    // Creating FIFO (only 1 for server to read from all clients)
-
-    umask(0);       /* So we get the permissions we want */
+    umask(0); /* So we get the permissions we want */
     
-    if (mkfifo(SERVER_FIFO, S_IRUSR | S_IWUSR | S_IWGRP) == -1 
+    snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) getpid());
+
+    if (mkfifo(clientFifo, S_IRUSR | S_IWUSR | S_IWGRP) == -1
         && errno != EEXIST)
     {
         fprintf(stderr, "ERROR: mk(client)fifo. ERROR IS NOT EEXIST\n");
         exit(MKFIFO_NO_EEXIT);
-    } 
+    }   
 
-    serverFd = open(SERVER_FIFO, O_RDONLY);
+    if (atexit(removeFifo) != 0)
+    {
+        fprintf(stderr, "ERROR: mk(client)fifo. ERROR IS NOT EEXIST\n");
+        exit(MKFIFO_NO_EEXIT);
+    }  
+
+    /* Construct request message, open server FIFO, and send request */
+    req.pid = getpid();
+    req.inNum = getNumber(argv[1]);
+
+    serverFd = open(SERVER_FIFO, O_WRONLY);
+
     if (serverFd == -1)
     {
-        fprintf(stderr, "ERRORopen %s", SERVER_FIFO);
-        exit(ERROR_OPEN_TO_READ_CLIENT);
+        fprintf(stderr, "ERROR in open on write server FIFO\n");
+        exit(EXIT_FAILURE); // add special error-name
     }
 
-    // fix EOF stuff
 
-    fixFd = open(SERVER_FIFO, O_WRONLY);
-
-    if (fixFd  == -1)
+    if ( write(serverFd, &req, sizeof(struct request)) != sizeof(struct request) )
     {
-        fprintf(stderr, "ERROR: open fixFD. %s\n", SERVER_FIFO);
-        exit(OPEN_FIX_FD);
+        fprintf(stderr, "Cant write to server FIFO\n");
+        exit(EXIT_FAILURE); // add special error-name
     }
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+    /* Open our FIFO, read and display response */
+
+    clientFd = open(clientFifo, O_RDONLY);
+
+    if (clientFd == -1)
     {
-        fprintf(stderr, "Can't set ignore to do in signal SISPIPE.\n");
-        exit(SIGPIPE_IGN_ERROR);
+        fprintf(stderr, "can't open clientFIFO to read\n");
+        exit(EXIT_FAILURE); // add special error-name
     }
     
+    //sleep(1);
 
-    // Getting REQUEST AND SENDING RESPONSE
-    resp.NOappeal = 0;
+    int NOread;
 
-    while(1)
+    if ( (NOread = read(clientFd, &resp, sizeof(struct response)) ) != sizeof(struct response) )
     {
-        // trying to read request from client until we read it  
-
-        if(read(serverFd, &req, sizeof(struct request)) != sizeof(struct request))
-        {
-            fprintf(stderr, "Not full read from client or error, continue to read.\n");
-            continue;
-        }
-
-        // get FD of exclusive client FIFO (see the pic in README) to answer him
-
-        snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) req.pid); // get unique name for client-read from server FIFO
-        
-        clientFd = open(clientFifo, O_WRONLY);
-        if (clientFd == -1)         /* Open failed, give up on client */
-        {
-            fprintf(stderr, "client FIFO failed to open."); 
-            continue;
-        }
-
-        /// Now server can send response to client by client FIFO
-
-        resp.sum = req.inNum + req.pid;
-        resp.NOappeal += 1;
-
-        if ( write(clientFd, &resp, sizeof(struct response)) != sizeof(struct response) )
-        {
-            fprintf(stderr, "Error in write to client\n");
-            exit(WRITE_TO_CLIENT);
-        }
-
-        printf("SERVER: size of write response = %d\n", sizeof(struct response));
-
-        if ( close(clientFd) != 0 )
-        {
-            fprintf(stderr, "Error in close client FD\n");
-        }
-
-
+        fprintf(stderr, "Can't get answer from server. NOread = %d (need = %d)\n", NOread, sizeof(struct response));
+        exit(EXIT_FAILURE); // add special error-name
     }
-    ///
+
+    printf("sum = %d\n", resp.sum);
+    printf("NO appeals = %d\n", resp.NOappeal);   
 
 ///
     exit(EXIT_SUCCESS);
