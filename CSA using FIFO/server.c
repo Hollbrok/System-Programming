c403bf4b419f53bb9dd46582f34e276a046f5c09
Hollbrok <talashkevich.da@phystech.edu>
2021-09-26 22:09:58 +0300

    Update server functional

--- CSA using FIFO/server.c
+++ CSA using FIFO/server.c
@@ -11,20 +11,23 @@
 #define CLIENT_FIFO_NAME_LEN (sizeof(CLIENT_FIFO_TEMPLATE) + 20)    /* Space required for client FIFO pathname
                                                                     (+20 as a generous allowance for the PID) */
 
+const int TRUE  = 1;
+const int FALSE = 0;
+
 static int NO_APPEALS; 
 
 struct request      /* Request (client --> server) */       
 {                 
     pid_t pid;      /* PID of client */
-    int inNum;     /* Length of desired sequence */ 
+    int inNum;      /* input number from cmd line */ 
 };
 
 
 
 struct response     /* Response (server --> client) */
 {   
-    int sum;     /* Start of sequence */
-    int NOappeal;
+    int sum;        
+    int NOappeals;
 };
 
 
@@ -33,9 +36,6 @@ struct response     /* Response (server --> client) */
 
 enum ERRORS_HANDLER
 {
-    NOT_ENOUGH_ARGUMENTS = -5,
-    TO_MUCH_ARGUMENTS       ,
-    ZEROSTRING_ARGV         ,
     MKFIFO_NO_EEXIT         ,
     OPEN_FIX_FD             ,
     SIGPIPE_IGN_ERROR       ,
@@ -46,186 +46,86 @@ enum ERRORS_HANDLER
 
 };
 
-long getNumber(const char *numString);
+static long getNumber(const char *numString);
 
-void handlerFIFO(int sig);
+static void handlerFIFO(int sig);
 
-static void updateBackUpFile(void)
-{
-    // Update NO_APPEALS
+static void updateBackUpFile(void);
 
-    int backupFd;
-    if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR)) == -1) 
-    {
-        if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR | O_CREAT, S_IRWXU)) == -1)
-        {
-            perror("Can't create backup file");
-            exit(EXIT_FAILURE);
-        }
-    }
+static void setSignalsHandler();
 
-    if( lseek(backupFd, 0, SEEK_SET) == -1)
-    {
-        perror("lseek");
-    }
+static int getBackupData(); 
 
-    if(write(backupFd, &NO_APPEALS, sizeof(int)) != sizeof(int))
-    {
-        perror("ERROR in updating backup file");
-    }
+static void fixFifoEof();
 
-    if( close(backupFd) == -1)
-    {
-        perror("ERROR in close backup file");
-    }
+static void createServerFIFO();
 
-}
+static int getRDofFIFO();
 
 int main(int argc, const char *argv[])
 {
-/// 
+    int serverRFd = -1;             /* read client request              */
+    int clientFd = -1;              /* read server response             */
 
-    int serverFd = -1;      // read client request
-    int clientFd = -1;      // read server response
-
-    int fixFd  = -1;        // this is FD to fix problem with FIFO: it needs to open FIFO write end to not meet EOF.
-
-    struct request req;     // request to server from client
-    struct response resp;   // answer to client from server
+    struct request req   = {};      /* request to server from client    */
+    struct response resp = {};      /* answer to client from server     */
 
     char clientFifo[CLIENT_FIFO_NAME_LEN];
 
-// Set signal handlers
+/* Set signals (INT + TERM) handlers                            */
 
-    if ( (signal(SIGINT, handlerFIFO) == SIG_ERR) || (signal(SIGTERM, handlerFIFO) == SIG_ERR) )
-    {
-        fprintf(stderr, "Can't set signal handler for SIGINT or SIGTERM\n");
-        exit(SET_SIGHANDLER_ERROR);
-    }
+    setSignalsHandler();
 
-//
+/* Get data from backup file (Number of appeals)                */
 
-    int backupFd;
-    if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR)) == -1) 
-    {
-        if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR | O_CREAT, S_IRWXU)) == -1)
-        {
-            perror("Can't create backup file");
-            exit(EXIT_FAILURE);
-        }
-    }
+    resp.NOappeals = getBackupData();
+    NO_APPEALS = resp.NOappeals;
 
-    if (atexit(updateBackUpFile) != 0)
-    {
-        fprintf(stderr, "Can't set exit function\n");
-        exit(EXIT_FAILURE);
-    }  
+
+/* Creating FIFO (only 1 for server to read from all clients)   */
+
+    createServerFIFO();
+
+
+/* fix that fifo may meet EOF if there are no clients           */
+
+    fixFifoEof();
     
+/* Get FD to read from serverFIFO                               */
 
-    struct stat st  = {}; 
-    int backupFSize = 0;
+    serverRFd = getRDofFIFO();
 
-    if (stat(SERVER_BACKUP_FILE, &st) == 0)
-        backupFSize = st.st_size;
+/* Getting Request from client                                  */
 
-    if (backupFSize == 0)
+    while (TRUE)
     {
-        fprintf(stderr, "TEST: backupFSize = 0\n");
-        resp.NOappeal = 0;
-    }
-    else
-    {
-        fprintf(stderr, "TEST: backupFSize = %d\n", backupFSize);
-        if( read(backupFd, &resp.NOappeal, sizeof(int)) != sizeof(int))
-        {
-            perror("Can't read data from backup file");
-            exit(EXIT_FAILURE); // or just = 0, not EXIT
-        }
-        fprintf(stderr, "TEST: read resp.NOappeal = %d\n", resp.NOappeal);
-    }
-//
-    // Creating FIFO (only 1 for server to read from all clients)
+        /* trying to read request from client until we got it                                           */
 
-    umask(0);       /* So we get the permissions we want */
-    errno = 0;
-
-    int mkfifoStatus = mkfifo(SERVER_FIFO, S_IRUSR | S_IWUSR | S_IWGRP);
-
-    while (mkfifoStatus == -1)
-    {
-        perror("\nTEST: MKFIFO returns -1\nTEST: errno");
-
-        if( unlink(SERVER_FIFO) == -1)
-        {
-            perror("Can't unlink FIFO, exit\n");
-            exit(EXIT_FAILURE);
-        }
-
-        //fprintf(stderr, "SUCCESS unlink FIFO, creating new..\n");
-        //exit(EXIT_FAILURE);
-
-        if(errno != EEXIST)
-        {
-            fprintf(stderr, "ERROR: mk(client)fifo. ERROR IS NOT EEXIST\n");
-            exit(MKFIFO_NO_EEXIT);
-        } 
-
-        mkfifoStatus = mkfifo(SERVER_FIFO, S_IRUSR | S_IWUSR | S_IWGRP);
-    }
-
-    serverFd = open(SERVER_FIFO, O_RDONLY);
-    if (serverFd == -1)
-    {
-        fprintf(stderr, "ERRORopen %s", SERVER_FIFO);
-        exit(ERROR_OPEN_TO_READ_CLIENT);
-    }
-
-    // fix EOF stuff
-
-    fixFd = open(SERVER_FIFO, O_WRONLY);
-
-    if (fixFd  == -1)
-    {
-        fprintf(stderr, "ERROR: open fixFD. %s\n", SERVER_FIFO);
-        exit(OPEN_FIX_FD);
-    }
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
-    {
-        fprintf(stderr, "Can't set ignore to do in signal SISPIPE.\n");
-        exit(SIGPIPE_IGN_ERROR);
-    }
-    
-
-    // Getting REQUEST AND SENDING RESPONSE
-    //resp.NOappeal = 0;
-
-    while(1)
-    {
-        // trying to read request from client until we read it  
-
-        if(read(serverFd, &req, sizeof(struct request)) != sizeof(struct request))
+        if (read(serverRFd, &req, sizeof(struct request)) != sizeof(struct request))
         {
             fprintf(stderr, "Not full read from client or error, continue to read.\n");
             continue;
         }
 
-        // get FD of exclusive client FIFO (see the pic in README) to answer him
+        /* generate unique name for (client-read from server) FIFO */
 
-        snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) req.pid); // get unique name for client-read from server FIFO
+        snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) req.pid); 
         
-        clientFd = open(clientFifo, O_WRONLY);
-        if (clientFd == -1)         /* Open failed, give up on client */
+        /* get FD of exclusive client FIFO (see the pic in README) to answer to client                  */
+
+        clientFd = open(clientFifo, O_WRONLY); /* Open failed, give up on client, reading the next data */
+        if (clientFd == -1)         
         {
             fprintf(stderr, "client FIFO failed to open."); 
             continue;
         }
 
-        /// Now server can send response to client by client FIFO
+        /* Now server can send response to client by client FIFO                                        */
 
         resp.sum = req.inNum + req.pid;
-        resp.NOappeal += 1;
+        resp.NOappeals += 1;
 
-        NO_APPEALS = resp.NOappeal;
+        NO_APPEALS = resp.NOappeals;
 
         if ( write(clientFd, &resp, sizeof(struct response)) != sizeof(struct response) )
         {
@@ -239,8 +139,6 @@ int main(int argc, const char *argv[])
         {
             fprintf(stderr, "Error in close client FD\n");
         }
-
-
     }
     ///
 
@@ -248,7 +146,7 @@ int main(int argc, const char *argv[])
     exit(EXIT_SUCCESS);
 }
 
-long getNumber(const char *numString)
+static long getNumber(const char *numString)
 {
     if (*numString == '\0')
     {
@@ -279,11 +177,154 @@ long getNumber(const char *numString)
 
 }
 
-void handlerFIFO(int sig)
+static void handlerFIFO(int sig)
 {
     unlink(SERVER_FIFO);
 
     fprintf(stderr, "FIFO handler got SIGNAL: %s\nTERMINATE\n", strsignal(sig));
 
     exit(FIFO_HANDLER_SIGNALS);
+}
+
+static void updateBackUpFile(void)
+{
+    // Update NO_APPEALS -- global const
+
+    int backupFd;
+    if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR)) == -1) 
+    {
+        if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR | O_CREAT, S_IRWXU)) == -1)
+        {
+            perror("Can't create backup file");
+            return;
+        }
+    }
+
+    if( lseek(backupFd, 0, SEEK_SET) == -1)
+    {
+        perror("lseek");
+        return;
+    }
+
+    if(write(backupFd, &NO_APPEALS, sizeof(int)) != sizeof(int))
+    {
+        perror("ERROR in updating backup file");
+        return;
+    }
+
+    if( close(backupFd) == -1)
+    {
+        perror("ERROR in close backup file");
+    }
+
+}
+
+static void setSignalsHandler()
+{
+    if ( (signal(SIGINT, handlerFIFO) == SIG_ERR) || (signal(SIGTERM, handlerFIFO) == SIG_ERR) )
+    {
+        fprintf(stderr, "Can't set signal handler for SIGINT or SIGTERM\n");
+        exit(SET_SIGHANDLER_ERROR);
+    }
+}
+
+static int getBackupData()
+{
+    int backupFd;
+    if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR)) == -1) 
+    {
+        if ( (backupFd = open(SERVER_BACKUP_FILE, O_SYNC | O_RDWR | O_CREAT, S_IRWXU)) == -1)
+        {
+            perror("Can't create backup file");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (atexit(updateBackUpFile) != 0)
+    {
+        fprintf(stderr, "Can't set exit function\n");
+        exit(EXIT_FAILURE);
+    }  
+    
+
+    struct stat st  = {}; 
+    int backupFSize = 0;
+
+    if (stat(SERVER_BACKUP_FILE, &st) == 0)
+        backupFSize = st.st_size;
+
+    int NOappeals = 0;
+
+    if (backupFSize == 0)
+    {
+        fprintf(stderr, "TEST: backupFSize = 0\n");
+        return NOappeals;
+    }
+    else
+    {
+        fprintf(stderr, "TEST: backupFSize = %d\n", backupFSize);
+        if( read(backupFd, &NOappeals, sizeof(int)) != sizeof(int))
+        {
+            perror("Can't read data from backup file");
+            exit(EXIT_FAILURE); // or just = 0, not EXIT
+        }
+        fprintf(stderr, "TEST: read resp.NOappeals = %d\n", NOappeals);
+    }
+
+    return NOappeals;
+}
+
+static void fixFifoEof()
+{
+    int fixFd  = -1;    /* this is FD to fix problem with FIFO: it needs to open FIFO write end to not meet EOF. */
+
+    fixFd = open(SERVER_FIFO, O_WRONLY);
+
+    if (fixFd  == -1)
+    {
+        fprintf(stderr, "ERROR: open fixFD. %s\n", SERVER_FIFO);
+        exit(OPEN_FIX_FD);
+    }
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+    {
+        fprintf(stderr, "Can't set ignore to do in signal SISPIPE.\n");
+        exit(SIGPIPE_IGN_ERROR);
+    }
+}
+
+static void createServerFIFO()
+{
+    int mkfifoStatus = mkfifo(SERVER_FIFO, S_IRUSR | S_IWUSR | S_IWGRP);
+
+    while (mkfifoStatus == -1)
+    {
+        perror("\nTEST: MKFIFO returns -1\nTEST: errno");
+
+        if( unlink(SERVER_FIFO) == -1)
+        {
+            perror("Can't unlink FIFO, exit\n");
+            exit(EXIT_FAILURE);
+        }
+
+        if(errno != EEXIST)
+        {
+            fprintf(stderr, "ERROR: mk(client)fifo. ERROR IS NOT EEXIST\n");
+            exit(MKFIFO_NO_EEXIT);
+        } 
+
+        mkfifoStatus = mkfifo(SERVER_FIFO, S_IRUSR | S_IWUSR | S_IWGRP);
+    }
+
+}
+
+static int getRDofFIFO()
+{
+    int serverRFd = open(SERVER_FIFO, O_RDONLY);
+    if (serverRFd == -1)
+    {
+        fprintf(stderr, "ERRORopen %s", SERVER_FIFO);
+        exit(ERROR_OPEN_TO_READ_CLIENT);
+    }
+
+    return serverRFd;
 }
