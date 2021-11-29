013f1119ff9fb83ad2d1364ae3676d9a033d3c62
Hollbrok <talashkevich.da@phystech.edu>
2021-11-29 15:46:35 +0300

    Add start set of files for project

---
+++ 5. Pipes/main.c
@@ -0,0 +1,14 @@
+#include "libs.h"
+#include "debug.h"
+#include "common.h"
+
+
+int main(int argc, const char *argv[])
+{
+    if (argc < 2)
+        err(EX_USAGE, "program needs name-of-file argument");
+
+
+
+    exit(EXIT_SUCCESS);
+}
