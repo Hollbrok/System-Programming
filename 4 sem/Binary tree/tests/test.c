e723cf8c77bb734c8b4dc9a193635a515ed50406
Hollbrok <talashkevich.da@phystech.edu>
2022-02-15 02:18:20 +0300

    add more tests for code covering

--- 4 sem/Binary tree/tests/test.c
+++ 4 sem/Binary tree/tests/test.c
@@ -2,54 +2,79 @@
 
 int printElem(struct bintreeElem *elem, void *x);
 
+void testAdd(struct bintree* tree);
+
 void testForeach(struct bintree *tree);
 
+void testConstrDeconstr();
 
-////////
-
-int main()
+int main(int argc, char *argv[])
 {
-    struct bintree *tree = (struct bintree *) (calloc(1, sizeof(struct bintree)));
-    if (tree == NULL)
-    {
-        fprintf(stderr, "can't calloc memory for tree.\n");
-        exit(ERR_CALLOC);
-    }
+    struct bintree tree = {};
 
-    createTree(tree);
 
-    add(tree, 5);
+    createTree(&tree);
 
-    fprintf(stderr, "size  = %d\n"
-                    "&root = %p\n", tree->size_, tree->root_);
+    
+    testAdd(&tree);
 
-    testForeach(tree);
+    testForeach(&tree);
 
-    //show_tree(tree);
+    testConstrDeconstr();
 
-    clear(tree);
-
+    clear(&tree);
     exit(EXIT_SUCCESS);
+}
+
+void testAdd(struct bintree* tree)
+{
+    add(tree, 5);
+    add(tree, 2);
+    add(tree, 3);
+    add(tree, 6);
+    add(tree, 100);
+    add(tree, 102);
+    add(tree, 0);
+    add(tree, -1);
+    add(tree, 66);
+    add(tree, 7);
+
+
+    add(NULL, 1);
 }
 
 int printElem(struct bintreeElem *elem, void *x)
 {
     if (elem->data_ > (int) x)
         printf("[%d]\n", elem->data_);
-
     return 0;
 }
 
-void testForeach(struct bintree *tree)
+void testForeach(struct bintree *tree) 
 {
     fprintf(stderr, "TTT: %p\n", tree->root_);
     for (int i = 0; i < 10; i++)
-        if (foreach(OR_T_INORDER, tree, printElem, (void *) -1) != ERR_SUCCESS)
-        {
-            fprintf(stderr, "[%d].FOREACH test FAILED.\n", i);
-            return;
-        }
+    {
+        foreach(OR_T_INORDER, tree, printElem, (void *) -1);
+        foreach(OR_T_PREORDER, tree, printElem, (void *) -1);
+        foreach(OR_T_POSTORDER, tree, printElem, (void *) -1);
+        foreach(OR_T_DEFAULT, tree, printElem, (void *) -1);
+    }
+
+    struct bintree treeNullRoot = {.root_ = NULL}; 
+
+    foreach(OR_T_DEFAULT, NULL, printElem, NULL);
+    foreach(OR_T_DEFAULT, &treeNullRoot, printElem, NULL);
+    foreach(OR_T_DEFAULT, tree, NULL, NULL);
     
     fprintf(stderr, "FOREACH test SUCCESS.\n");
     return;
 }
+
+void testConstrDeconstr()
+{
+    createElem(NULL, 1);
+    deconstrElem(NULL);
+    createTree(NULL);
+}
+
