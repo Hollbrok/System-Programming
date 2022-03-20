b1a7cca09f5cd8994f8ecf397702d1a678dc8c41
Hollbrok <talashkevich.da@phystech.edu>
2022-03-20 17:55:47 +0300

    add shell script to test speed of calculating integral

---
+++ 4 sem/Threads-int/test_speed.sh
@@ -0,0 +1,20 @@
+echo -e "Start of testing speed: \n"
+
+start_NO_threads=1
+
+DEFAULT_MAX_NO_THREADS=16
+max_NO_threads=${1:-${DEFAULT_MAX_NO_THREADS}}
+
+# cycle of running calculation in threads (from 1 to 12) 
+# my pc has 8 cores so the time result will decrease until the number of threads reaches 8
+# after time should not increase 
+
+
+for NO_threads in $(eval echo "{$start_NO_threads..$max_NO_threads}")
+do
+    echo "NO threads: $NO_threads"
+    time ./xxx $NO_threads
+    echo -e "\n"
+done
+
+echo -e "END OF TESTS\n\n"
