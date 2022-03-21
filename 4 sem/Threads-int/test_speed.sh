1861da695a9d7b8c099dc44fecc2a4e48cefefdb
Hollbrok <talashkevich.da@phystech.edu>
2022-03-21 14:08:36 +0300

    add comment how to get full info; change format of output to minimalistic

--- 4 sem/Threads-int/test_speed.sh
+++ 4 sem/Threads-int/test_speed.sh
@@ -13,7 +13,8 @@ max_NO_threads=${1:-${DEFAULT_MAX_NO_THREADS}}
 for NO_threads in $(eval echo "{$start_NO_threads..$max_NO_threads}")
 do
     echo "NO threads: $NO_threads"
-    time ./xxx $NO_threads
+    # flag -v for full info
+    /usr/bin/time -f "\n\tcalculation time: %e sec\n" ./xxx $NO_threads
     echo -e "\n"
 done
 
