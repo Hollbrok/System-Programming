acd6cbb87ff621776018911d111046e3021067ca
Hollbrok <talashkevich.da@phystech.edu>
2022-03-22 12:38:32 +0300

    some changes in logic: delete deltaX from ThreadInfo; and moved some initialization stuff to initThreadInfo

--- 4 sem/Threads-int/threads_int_func.c
+++ 4 sem/Threads-int/threads_int_func.c
@@ -10,6 +10,11 @@ double calcInt(char *strNum, IntFunc intFunc)
 
     /* usr number of threads to calculate int */
     int noThreads = getNumber(strNum);
+    if (noThreads <= 0)
+    {
+        fprintf(stderr, "NO threads must be > 0\n");
+        return NAN;
+    }
 
     /* get number of processors currently available in the system */
     int noProc = sysconf(_SC_NPROCESSORS_ONLN);
@@ -29,9 +34,9 @@ double calcInt(char *strNum, IntFunc intFunc)
         ERR_HANDLER("AllocThreadInfo");
 
 
-    /*  */
+    /* comment will be here */
 
-    pthread_t* threadsID = (pthread_t*)calloc(maxThreads, sizeof(pthread_t));
+    pthread_t* threadsID = (pthread_t *)calloc(maxThreads, sizeof(pthread_t));
     if (threadsID == NULL)
         ERR_HANDLER("Can`t calloc memory for threadsID");
 
@@ -43,9 +48,6 @@ double calcInt(char *strNum, IntFunc intFunc)
 
     for (int iThread = 0; iThread < maxThreads; iThread++) 
     {
-        if (noEmptyThreads > 0)
-            ((ThreadInfo*)(threadsInfo + iThread * sizeThreadInfo))->numCPU = iThread;
-
         if (pthread_create(threadsID + iThread, NULL, 
                            pthreadStartFunc, (threadsInfo + iThread * sizeThreadInfo)) != 0)
             ERR_HANDLER("Can`t create thread");
@@ -62,18 +64,10 @@ double calcInt(char *strNum, IntFunc intFunc)
             ERR_HANDLER("pthread_join non-empty Threads");
 
         if (iThread < noThreads)
-            finalSum += ((ThreadInfo*)(threadsInfo + iThread * sizeThreadInfo))->sum;
+            finalSum += ((ThreadInfo *)(threadsInfo + iThread * sizeThreadInfo))->sum;
     }
 
-    /*for (int iThread = noEmptyThreads; iThread < maxThreads; iThread++) 
-    {
-        if (pthread_join(threadsID[iThread], NULL) != 0)
-            ERR_HANDLER("pthread_join empty Thread");
-    }*/
-
     /* printing result and exit s*/
-
-    //fprintf(stdout,"\tIntegral value - %lg", finalSum);
 
     free(threadsInfo);
     free(threadsID);
@@ -97,18 +91,19 @@ static void initThreadsInfo(void *info, size_t sizeThreadInfo, int noThreads,
 {
     /* general initialization stuff */
     for (int iThread = 0; iThread < noThreads + noEmptyThreads; ++iThread)
-    {
-        ((ThreadInfo*) (info + iThread * sizeThreadInfo))->dX = DELTA_X;
-    
-        ((ThreadInfo*) (info + iThread * sizeThreadInfo))->numCPU = -1;
+    {    
+        if (noEmptyThreads > 0)
+            ((ThreadInfo *)(info + iThread * sizeThreadInfo))->numCPU = iThread;
+        else
+            ((ThreadInfo *) (info + iThread * sizeThreadInfo))->numCPU = -1;
     }
 
     /* for ordered threads */
     for (int iThread = 0; iThread < noThreads; iThread++) 
     {
-        ((ThreadInfo*) (info + iThread * sizeThreadInfo))->a = START_LIMIT + iThread * intLength;
+        ((ThreadInfo *) (info + iThread * sizeThreadInfo))->a = START_LIMIT + iThread * intLength;
 
-        ((ThreadInfo*) (info + iThread * sizeThreadInfo))->b = ((ThreadInfo *)(info + iThread * sizeThreadInfo))->a + intLength;
+        ((ThreadInfo *) (info + iThread * sizeThreadInfo))->b = ((ThreadInfo *)(info + iThread * sizeThreadInfo))->a + intLength;
     }
 
     /* for empty threads */
@@ -118,6 +113,7 @@ static void initThreadsInfo(void *info, size_t sizeThreadInfo, int noThreads,
         
         ((ThreadInfo*) (info + iThread * sizeThreadInfo))->b = START_LIMIT + intLength;
     }
+    
 }
 
 static void *pthreadStartFunc(void* arg)
@@ -141,27 +137,26 @@ static void *pthreadStartFunc(void* arg)
     }
 
     /* calculate int */
-    for (double x = threadsInfo->a; x < threadsInfo->b; x += threadsInfo->dX)
-        threadsInfo->sum += function(x) * threadsInfo->dX;
+    for (double x = threadsInfo->a; x < threadsInfo->b; x += DELTA_X)
+        threadsInfo->sum += function(x) * DELTA_X;
 
     return NULL;
 }
 
 static long getNumber(char *numString)
 {
-
     if (*numString == '\0')
     {
         fprintf(stderr, "empty number argument\n");
         exit(EXIT_FAILURE);
     }
 
-    errno = 0;
-
     long gNumber;
     char* endOfEnter;
 
     const int baseOfNumber = 10;
+    errno = 0;
+
     gNumber = strtol(numString, &endOfEnter, baseOfNumber);
 
     if(*endOfEnter != '\0')
