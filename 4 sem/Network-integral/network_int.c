28c55bb9c31adae3159f806d09ba2d21509680ef
Hollbrok <talashkevich.da@phystech.edu>
2022-03-26 20:10:07 +0300

    add main .c file

---
+++ 4 sem/Network-integral/network_int.c
@@ -0,0 +1,22 @@
+#include "network_int.h"
+
+/* integral of what function will be calculated */
+double function(double x)
+{
+    return cos( pow(x, 5) * sin(atan(x)) );  
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        ERR_HANDLER("use: ./xxx <NO PC>");
+
+    DEBPRINT("pid = %ld\n", (long)getpid());
+
+    double intVal = calcInt(argv[1], function);
+
+    fprintf(stdout,"\tIntegral value - %lg", intVal);
+
+    return EXIT_SUCCESS;
+}
+
