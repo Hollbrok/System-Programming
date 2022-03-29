981f2c9c203e178334594121548168aaf29e1242
Hollbrok <talashkevich.da@phystech.edu>
2022-03-29 17:17:46 +0300

    add libs and debug stuff

---
+++ 4 sem/Network-integral/Common/debug.h
@@ -0,0 +1,27 @@
+#pragma once
+
+/* debug and errs handling stuff */
+
+#define DEB_REGIME   0
+#define NEED_SLEEP   0
+#define NEED_LINE    1
+#define NEED_PID     0
+
+
+#define DEBPRINT(args...)                                               \
+    do {                                                                \
+        if(DEB_REGIME) {                                              \
+            if (NEED_LINE)                                              \
+                fprintf(stderr, "LINE: %d\n", __LINE__);                \
+            if (NEED_PID)                                               \
+                fprintf(stderr, "PID: %ld\n", (long) getpid());         \
+            fprintf(stderr, args);                                      \
+        }                                                               \
+    } while(0)
+
+
+#define ERR_HANDLER(msg)    \
+    do { perror(msg); exit(EXIT_FAILURE); } while(0)
+
+
+#define MAX(a, b) (a) > (b) ? (a) : (b)
---
+++ 4 sem/Network-integral/Common/libs.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <errno.h>
+
+#include <unistd.h>
+#include <stdbool.h>
+
+#include <pthread.h>
+
+#include <sys/sysinfo.h>
+#include <sys/types.h>
+
+#include <math.h>
