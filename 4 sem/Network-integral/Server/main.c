8b248282410380725229a4a42dfd33f43c975ae9
Hollbrok <talashkevich.da@phystech.edu>
2022-04-07 22:27:31 +0300

    add logic for client serving

--- 4 sem/Network-integral/Server/main.c
+++ 4 sem/Network-integral/Server/main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 void serverInt(int noPc, int noThreads)
 {
-    int					listenfd, connfd;
+    int					listenfd;
 	pid_t				childpid;
 	socklen_t			clilen;
 	struct sockaddr_in	cliaddr, servaddr;
@@ -40,12 +40,56 @@ void serverInt(int noPc, int noThreads)
 
 	Listen(listenfd, LISTENQ);
 
+    double totalIntResult = 0;
 
-	clilen = sizeof(cliaddr);
-	connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+    int connFds[noPc];
 
-    Writen(connfd, &noPc, sizeof(int));
+    /* waiting for all clients */
+    for (int iClient = 0; iClient < noPc; iClient++)
+    {
+        clilen = sizeof(cliaddr);
+	    connFds[iClient] = Accept(listenfd, (SA *) &cliaddr, &clilen);
 
-	close(connfd);			/* parent closes connected socket */
+        struct ReadyMsg getReady;
+        if (Readn(connFds[iClient], &getReady, sizeof(getReady)) != sizeof(getReady))
+            err_quit("Readn: server terminated prematurely (read ready msg)");
+    }
+
+    /* from now time can be counted */
+    struct timeval beginT, endT;
+    gettimeofday(&beginT, 0);
+
+    fprintf(stderr, "starting..\n");
+
+    /* send needed info for calc */
+    for (int iClient = 0; iClient < noPc; iClient++)
+    {
+        struct CalcInfo calcInfo = {
+            .iClient = iClient,
+            .noPc = noPc,
+            .noThreads = noThreads,
+        };
+        Writen(connFds[iClient], &calcInfo, sizeof(calcInfo));
+    }
+
+    /* get results */
+    for (int iClient = 0; iClient < noPc; iClient++)
+    {
+        struct IntResult intRes;
+        if (Readn(connFds[iClient], &intRes, sizeof(intRes)) != sizeof(intRes))
+            err_quit("Readn: server terminated prematurely (read int result)");
+
+        totalIntResult += intRes.result;
+
+	    close(connFds[iClient]);
+    }
+
+    gettimeofday(&endT, 0);
+    long seconds = endT.tv_sec - beginT.tv_sec;
+    long microseconds = endT.tv_usec - beginT.tv_usec;
+    double elapsed = seconds + microseconds*1e-6;
+
+	fprintf(stderr, "result = %lg\n"
+                    "time: %.3f secs.\n", totalIntResult, elapsed);
     close(listenfd);
 }
