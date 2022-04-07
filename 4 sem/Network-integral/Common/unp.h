48a20d23716413d7e78ee55e5a8b1549e207cee8
Hollbrok <talashkevich.da@phystech.edu>
2022-04-07 22:27:12 +0300

    add structures for data transmission and lib for time

--- 4 sem/Network-integral/Common/unp.h
+++ 4 sem/Network-integral/Common/unp.h
@@ -1,24 +1,12 @@
-/* include unph */
-/* Our own header.  Tabs are set for 4 spaces, not 8 */
-
 #ifndef	__unp_h
 #define	__unp_h
 
-/* If anything changes in the following list of #includes, must change
-   acsite.m4 also, for configure's tests. */
 
 #include	<sys/types.h>	/* basic system data types */
 #include	<sys/socket.h>	/* basic socket definitions */
-#if TIME_WITH_SYS_TIME
-#include	<sys/time.h>	/* timeval{} for select() */
-#include	<time.h>		/* timespec{} for pselect() */
-#else
-#if HAVE_SYS_TIME_H
-#include	<sys/time.h>	/* includes <time.h> unsafely */
-#else
+
 #include	<time.h>		/* old system? */
-#endif
-#endif
+
 #include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
 #include	<arpa/inet.h>	/* inet(3) functions */
 #include	<errno.h>
@@ -34,71 +22,15 @@
 #include	<sys/wait.h>
 #include	<sys/un.h>		/* for Unix domain sockets */
 
-#include <math.h>
-
-
-#ifdef	HAVE_SYS_SELECT_H
-# include	<sys/select.h>	/* for convenience */
-#endif
-
-#ifdef	HAVE_SYS_SYSCTL_H
-#ifdef	HAVE_SYS_PARAM_H
-# include	<sys/param.h>	/* OpenBSD prereq for sysctl.h */
-#endif
-//# include	<sys/sysctl.h>
-#endif
-
-#ifdef	HAVE_POLL_H
-# include	<poll.h>		/* for convenience */
-#endif
-
-#ifdef	HAVE_SYS_EVENT_H
-//# include	<sys/event.h>	/* for kqueue */
-#endif
-
-#ifdef	HAVE_STRINGS_H
-# include	<strings.h>		/* for convenience */
-#endif
+#include    <sys/time.h>
+#include    <math.h>
 
 
 
-#ifndef	INADDR_NONE
-/* $$.Ic INADDR_NONE$$ */
-#define	INADDR_NONE	0xffffffff	/* should have been in <netinet/in.h> */
-#endif
-
-#ifndef	SHUT_RD				/* these three POSIX names are new */
-#define	SHUT_RD		0	/* shutdown for reading */
-#define	SHUT_WR		1	/* shutdown for writing */
-#define	SHUT_RDWR	2	/* shutdown for reading and writing */
-/* $$.Ic SHUT_RD$$ */
-/* $$.Ic SHUT_WR$$ */
-/* $$.Ic SHUT_RDWR$$ */
-#endif
-
-/* *INDENT-OFF* */
-#ifndef INET_ADDRSTRLEN
-/* $$.Ic INET_ADDRSTRLEN$$ */
-#define	INET_ADDRSTRLEN		16	/* "ddd.ddd.ddd.ddd\0"
-								    1234567890123456 */
-#endif
-
-/* Define following even if IPv6 not supported, so we can always allocate
-   an adequately sized buffer without #ifdefs in the code. */
-#ifndef INET6_ADDRSTRLEN
-/* $$.Ic INET6_ADDRSTRLEN$$ */
-#define	INET6_ADDRSTRLEN	46	/* max size of IPv6 address string:
-				   "xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:xxxx" or
-				   "xxxx:xxxx:xxxx:xxxx:xxxx:xxxx:ddd.ddd.ddd.ddd\0"
-				    1234567890123456789012345678901234567890123456 */
-#endif
-/* *INDENT-ON* */
 
 /* Define bzero() as a macro if it's not in standard C library. */
 #ifndef	HAVE_BZERO
 #define	bzero(ptr,n)		memset(ptr, 0, n)
-/* $$.If bzero$$ */
-/* $$.If memset$$ */
 #endif
 
 
@@ -123,19 +55,29 @@
 /* Define some port number that can be used for our examples */
 #define	SERV_PORT		 9878			/* TCP and UDP */
 #define	SERV_PORT_STR	"9878"			/* TCP and UDP */
-#define	UNIXSTR_PATH	"/tmp/unix.str"	/* Unix domain stream */
-#define	UNIXDG_PATH		"/tmp/unix.dg"	/* Unix domain datagram */
-
-#define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
-					/* default file access permissions for new files */
-#define	DIR_MODE	(FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
-					/* default permissions for new directories */
 
 #define	min(a,b)	((a) < (b) ? (a) : (b))
 #define	max(a,b)	((a) > (b) ? (a) : (b))
 
 
-/* end unph */
+/* structures */
+
+struct ReadyMsg
+{
+    char r;
+};
+
+struct CalcInfo
+{
+    size_t noPc;
+    size_t noThreads;
+    size_t iClient;
+};
+
+struct IntResult
+{
+    double result;
+};
 
 
 /* cmd line number parser ( returns <long> number according to numString with error handling) */
@@ -146,13 +88,9 @@ ssize_t readline(int fd, void *vptr, size_t maxlen);
 ssize_t	 writen(int, const void *, size_t);
 
 
-#ifndef	HAVE_INET_PTON_PROTO
-int			 inet_pton(int, const char *, void *);
-//const char	*inet_ntop(int, const void *, char *, size_t);
-#endif
+int		inet_pton(int, const char *, void *);
 
 void    Inet_pton(int, const char *, void *);
-
 
 void    Write(int, void *, size_t);
 
